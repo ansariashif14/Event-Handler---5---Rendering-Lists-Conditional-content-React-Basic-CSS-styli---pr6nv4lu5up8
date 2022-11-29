46550b2e24187ff1ac805f28f03048f5d3011015
ansariashif14 <ansariashif14@gmail.com>
2022-11-29 19:30:58 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,22 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-
+const handleSubmit=(event)=>{
+  event.preventDefault();
+  console.log("form submitted");
+}
   return (
     <div id="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='name'>Name</label>
         <input id="name" type={"text"} />
 
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type='submit' onSubmit={handleSubmit}>Submit</button>
       </form>
     </div>
   )
 }
-
 
 export default App;
